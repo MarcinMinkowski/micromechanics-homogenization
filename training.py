19bd25a416c30db549dbbc830ccd7387bd0a1ebd
MarcinMinkowski <minkowski.m@gmail.com>
2026-06-26 09:35:36 +0300

    Added other u components to loss_pinn

--- training.py
+++ training.py
@@ -1,38 +1,38 @@
 import torch
 
-def residual(u, X, lam, mu):
+def residual(ux, uy, uz, X, lam, mu):
 
-    du1x = torch.autograd.grad(outputs=u[:,0],inputs=X,grad_outputs=torch.ones_like(u[:,0]),retain_graph=True,create_graph=True)[0]
-    du1y = torch.autograd.grad(outputs=u[:,1],inputs=X,grad_outputs=torch.ones_like(u[:,1]),retain_graph=True,create_graph=True)[0]
-    du1z = torch.autograd.grad(outputs=u[:,2],inputs=X,grad_outputs=torch.ones_like(u[:,2]),retain_graph=True,create_graph=True)[0]
+    dux = torch.autograd.grad(outputs=ux,inputs=X,grad_outputs=torch.ones_like(ux),retain_graph=True,create_graph=True)[0]
+    duy = torch.autograd.grad(outputs=uy,inputs=X,grad_outputs=torch.ones_like(uy),retain_graph=True,create_graph=True)[0]
+    duz = torch.autograd.grad(outputs=uz,inputs=X,grad_outputs=torch.ones_like(uz),retain_graph=True,create_graph=True)[0]
 
-    du1x_dx, du1x_dy, du1x_dz = du1x[:,0], du1x[:,1], du1x[:,2]
-    du1y_dx, du1y_dy, du1y_dz = du1y[:,0], du1y[:,1], du1y[:,2]
-    du1z_dx, du1z_dy, du1z_dz = du1z[:,0], du1z[:,1], du1z[:,2]
+    dux_dx, dux_dy, dux_dz = dux[:,0], dux[:,1], dux[:,2]
+    duy_dx, duy_dy, duy_dz = duy[:,0], duy[:,1], duy[:,2]
+    duz_dx, duz_dy, duz_dz = duz[:,0], duz[:,1], duz[:,2]
 
-    u1_div = du1x_dx + du1y_dy + du1z_dz
+    u_div = dux_dx + duy_dy + duz_dz
 
-    du1_div_d = torch.autograd.grad(outputs=u1_div,inputs=X,grad_outputs=torch.ones_like(u1_div),retain_graph=True)[0]
+    du_div_d = torch.autograd.grad(outputs=u_div,inputs=X,grad_outputs=torch.ones_like(u_div),retain_graph=True)[0]
 
-    du1x_dxd = torch.autograd.grad(outputs=du1x_dx,inputs=X,grad_outputs=torch.ones_like(du1x_dx),retain_graph=True)[0]
-    du1x_dyd = torch.autograd.grad(outputs=du1x_dy,inputs=X,grad_outputs=torch.ones_like(du1x_dy),retain_graph=True)[0]
-    du1x_dzd = torch.autograd.grad(outputs=du1x_dz,inputs=X,grad_outputs=torch.ones_like(du1x_dz),retain_graph=True)[0]
+    dux_dxd = torch.autograd.grad(outputs=dux_dx,inputs=X,grad_outputs=torch.ones_like(dux_dx),retain_graph=True)[0]
+    dux_dyd = torch.autograd.grad(outputs=dux_dy,inputs=X,grad_outputs=torch.ones_like(dux_dy),retain_graph=True)[0]
+    dux_dzd = torch.autograd.grad(outputs=dux_dz,inputs=X,grad_outputs=torch.ones_like(dux_dz),retain_graph=True)[0]
 
-    u1x_laplacian = du1x_dxd[:,0] + du1x_dyd[:,1] + du1x_dzd[:,2]
+    ux_laplacian = dux_dxd[:,0] + dux_dyd[:,1] + dux_dzd[:,2]
         
-    du1y_dxd = torch.autograd.grad(outputs=du1y_dx,inputs=X,grad_outputs=torch.ones_like(du1y_dx),retain_graph=True)[0]
-    du1y_dyd = torch.autograd.grad(outputs=du1y_dy,inputs=X,grad_outputs=torch.ones_like(du1y_dy),retain_graph=True)[0]
-    du1y_dzd = torch.autograd.grad(outputs=du1y_dz,inputs=X,grad_outputs=torch.ones_like(du1y_dz),retain_graph=True)[0]
+    duy_dxd = torch.autograd.grad(outputs=duy_dx,inputs=X,grad_outputs=torch.ones_like(duy_dx),retain_graph=True)[0]
+    duy_dyd = torch.autograd.grad(outputs=duy_dy,inputs=X,grad_outputs=torch.ones_like(duy_dy),retain_graph=True)[0]
+    duy_dzd = torch.autograd.grad(outputs=duy_dz,inputs=X,grad_outputs=torch.ones_like(duy_dz),retain_graph=True)[0]
 
-    u1y_laplacian = du1y_dxd[:,0] + du1y_dyd[:,1] + du1y_dzd[:,2]
+    uy_laplacian = duy_dxd[:,0] + duy_dyd[:,1] + duy_dzd[:,2]
 
-    du1z_dxd = torch.autograd.grad(outputs=du1z_dx,inputs=X,grad_outputs=torch.ones_like(du1z_dx),retain_graph=True)[0]
-    du1z_dyd = torch.autograd.grad(outputs=du1z_dy,inputs=X,grad_outputs=torch.ones_like(du1z_dy),retain_graph=True)[0]
-    du1z_dzd = torch.autograd.grad(outputs=du1z_dz,inputs=X,grad_outputs=torch.ones_like(du1z_dz),retain_graph=True)[0]
+    duz_dxd = torch.autograd.grad(outputs=duz_dx,inputs=X,grad_outputs=torch.ones_like(duz_dx),retain_graph=True)[0]
+    duz_dyd = torch.autograd.grad(outputs=duz_dy,inputs=X,grad_outputs=torch.ones_like(duz_dy),retain_graph=True)[0]
+    duz_dzd = torch.autograd.grad(outputs=duz_dz,inputs=X,grad_outputs=torch.ones_like(duz_dz),retain_graph=True)[0]
 
-    u1z_laplacian = du1z_dxd[:,0] + du1z_dyd[:,1] + du1z_dzd[:,2]
+    uz_laplacian = duz_dxd[:,0] + duz_dyd[:,1] + duz_dzd[:,2]
 
-    return (lam+mu)*du1_div_d[:,0]+mu*u1x_laplacian, (lam+mu)*du1_div_d[:,1]+mu*u1y_laplacian, (lam+mu)*du1_div_d[:,2]+mu*u1z_laplacian
+    return (lam+mu)*du_div_d[:,0]+mu*ux_laplacian, (lam+mu)*du_div_d[:,1]+mu*uy_laplacian, (lam+mu)*du_div_d[:,2]+mu*uz_laplacian
 
 def train_loop(data, model, dataloader, loss_fn, optimizer, device):
     loss_data_epoch = 0.0
@@ -62,9 +62,20 @@ def train_loop(data, model, dataloader, loss_fn, optimizer, device):
         mu = mu.to(device)
 
         u = model(point_pinn)
-        res_x, res_y, res_z = residual(u, point_pinn, lam, mu)
         
-        loss_pinn = loss_fn(res_x,torch.zeros_like(res_x)) + loss_fn(res_y,torch.zeros_like(res_y)) + loss_fn(res_z,torch.zeros_like(res_z))
+        res1_x, res1_y, res1_z = residual(u[:,0], u[:,1], u[:,2], point_pinn, lam, mu)
+        res2_x, res2_y, res2_z = residual(u[:,3], u[:,4], u[:,5], point_pinn, lam, mu)
+        res3_x, res3_y, res3_z = residual(u[:,6], u[:,7], u[:,8], point_pinn, lam, mu)
+        res4_x, res4_y, res4_z = residual(u[:,9], u[:,10], u[:,11], point_pinn, lam, mu)
+        res5_x, res5_y, res5_z = residual(u[:,12], u[:,13], u[:,14], point_pinn, lam, mu)
+        res6_x, res6_y, res6_z = residual(u[:,15], u[:,16], u[:,17], point_pinn, lam, mu)
+        
+        loss_pinn = loss_fn(res1_x,torch.zeros_like(res1_x)) + loss_fn(res1_y,torch.zeros_like(res1_y)) + loss_fn(res1_z,torch.zeros_like(res1_z)) + \
+                    loss_fn(res2_x,torch.zeros_like(res2_x)) + loss_fn(res2_y,torch.zeros_like(res2_y)) + loss_fn(res2_z,torch.zeros_like(res2_z)) + \
+                    loss_fn(res3_x,torch.zeros_like(res3_x)) + loss_fn(res3_y,torch.zeros_like(res3_y)) + loss_fn(res3_z,torch.zeros_like(res3_z)) + \
+                    loss_fn(res4_x,torch.zeros_like(res4_x)) + loss_fn(res4_y,torch.zeros_like(res4_y)) + loss_fn(res4_z,torch.zeros_like(res4_z)) + \
+                    loss_fn(res5_x,torch.zeros_like(res5_x)) + loss_fn(res5_y,torch.zeros_like(res5_y)) + loss_fn(res5_z,torch.zeros_like(res5_z)) + \
+                    loss_fn(res6_x,torch.zeros_like(res6_x)) + loss_fn(res6_y,torch.zeros_like(res6_y)) + loss_fn(res6_z,torch.zeros_like(res6_z))
 
         loss = loss_data + loss_pinn
         
